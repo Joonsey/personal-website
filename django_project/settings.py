4babe863102d5404f25298182a33f5025a8d18d0
Johannes Rønning <Jonseyplayz@gmail.com>
2021-12-10 04:59:08 +0000

    Hosted files on server and changed settings.

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kqwm^y&oj&-s2kh39r(1#@t(ujv-wucv-28q57oof#izyrt#$g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["139.162.160.227","jaes.life"]
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
