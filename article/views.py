f52f0b324aa7e0301fcc1f276a0cd75adbccedec
Johannes <jonseyplayz@gmail.com>
2021-12-12 12:51:57 +0100

    did some programming

--- article/views.py
+++ article/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def removeDashes(str):
+    return " ".join(str.split("-")).capitalize()
+
 def home(request, id):
-    return render(request, "article/%s.html" % id)
+    return render(request, "article/%s.html" % id, context= {'title' : removeDashes(id)})
